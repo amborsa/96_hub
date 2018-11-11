045a5b6bb6781f908c2e4bbe96954c28c95b1e4c
Adriano Borsa <amborsa@college.harvard.edu>
2018-11-11 12:47:06 -0500

    from my computer

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("layout.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
