a7d5cbec2775fc6abb15fa49f98c4773f6b5f83f
Adriano Borsa <amborsa@college.harvard.edu>
2018-11-13 14:36:32 -0500

    message

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 
+
 app = Flask(__name__)
 
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
 @app.route('/')
-def index():
-    # vitals = db.execute("SELECT * FROM Patient1 WHERE time = (SELECT MAX(time) FROM Patient1)")
-    # hr = vitals[0]["hr"]
-    # temp = vitals[0]["temp"]
-    # rr = vitals[0]["rr"]
-    # current_vital = [hr, temp, rr]
-    # ,vital = current_vital
+@nocache
+def main():
+	"""Show main page"""
+	"""At the very least, we will need to pass device ID,
+	patient information for every device."""
     return render_template("main.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
+
+
