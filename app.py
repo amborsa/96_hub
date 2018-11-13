b6dec98ee10d5873ca6433b1c3acbb5efd8f7204
Adriano Borsa <amborsa@college.harvard.edu>
2018-11-13 17:39:23 -0500

    message

--- app.py
+++ app.py
@@ -20,9 +20,15 @@ def main():
     """Show main page"""
 
     # this array should be selected from the database
-    ids=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    ids=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19]
+    names=["John", "Jim", "Jade", "John", "Jim", "Jade", "John", "Jim", "Jade", "John", "Jim", "Jade", "John", "Jim", "Jade", \
+    "John", "Jim"]
+    devices = []
+    for i in range(len(ids)):
+        new_dict = {id: ids[i], name: names[i]}
+        devices.append(new_dict)
 
-    return render_template("main.html", ids=ids)
+    return render_template("main.html", devices=devices)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
