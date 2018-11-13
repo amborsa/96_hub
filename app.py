0d484dda4221fb1950b0a34bec7fb219de946ed6
Adriano Borsa <amborsa@college.harvard.edu>
2018-11-13 17:07:38 -0500

    push

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
 	"""Show main page"""
 
 	# this array should be selected from the database
-    ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19]
+    ids=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
     return render_template("main.html", ids=ids)
 
 if __name__ == "__main__":
