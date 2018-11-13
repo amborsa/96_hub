b9ee654060a7b72f84e3071120a3667f78534aec
Adriano Borsa <amborsa@college.harvard.edu>
2018-11-13 14:45:28 -0500

    trying to fix caching

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 
 app = Flask(__name__)
 
+# caches should not be stored
 if app.config["DEBUG"]:
     @app.after_request
     def after_request(response):
@@ -10,6 +11,12 @@ if app.config["DEBUG"]:
         response.headers["Expires"] = 0
         response.headers["Pragma"] = "no-cache"
         return response
+
+# using filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def main():
